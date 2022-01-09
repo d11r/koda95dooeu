73b523e21ed90a1df9d5f4458815ace7f5b8c33a
Dragos Strugar <itsdragos@gmail.com>
2022-01-09 15:20:03 +0100

    pixel

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
+import {useRouter} from 'next/router'
+import {useEffect} from 'react'
+
 import '@styles/globals.css'
 
-function Application({ Component, pageProps }) {
+const App = ({Component, pageProps}) => {
+  const router = useRouter()
+
+  useEffect(() => {
+    import('react-facebook-pixel')
+      .then((x) => x.default)
+      .then((ReactPixel) => {
+        ReactPixel.init('378808883714993')
+        ReactPixel.pageView()
+
+        router.events.on('routeChangeComplete', () => {
+          ReactPixel.pageView()
+        })
+      })
+  }, [router.events])
+
   return <Component {...pageProps} />
 }
 
-export default Application
+export default App
