0093fa57b92360353168d5ecb2db737cfc89fdf2
Dragos Strugar <itsdragos@gmail.com>
2022-01-10 10:59:02 +0100

    add logo

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,19 @@
+import Image from 'next/image'
 import styles from './Footer.module.css'
+
+import Logo from './koda95.png'
 
 export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
+        <Image
+          src={Logo}
+          alt="KODA95 logo"
+          placeholder="KODA95 logo"
+          height={36}
+          width={36}
+        />
         KODA95doo. Sva prava zadrzana.
       </footer>
     </>
