308142da1b776f2cd68f62ba3e015ce71bb072d4
Dragos Strugar <itsdragos@gmail.com>
2022-01-10 10:31:54 +0100

    add arrow

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,3 @@
-export default function Header({ title }) {
+export default function Header({title}) {
   return <h1 className="title">{title}</h1>
 }
--- pages/index.js
+++ pages/index.js
@@ -2,17 +2,22 @@ import ReactPlayer from 'react-player'
 
 import Head from 'next/head'
 import Link from 'next/link'
+import dynamic from 'next/dynamic'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+
+const CurvedArrow = dynamic(() => import('react-curved-arrow'), {
+  ssr: false,
+})
 
 export default function Home() {
   const trackClick = () => {
     import('react-facebook-pixel')
-    .then((x) => x.default)
-    .then((ReactPixel) => {
-      ReactPixel.init('378808883714993')
-      ReactPixel.track('ViewContent')
-    })
+      .then((x) => x.default)
+      .then((ReactPixel) => {
+        ReactPixel.init('378808883714993')
+        ReactPixel.track('ViewContent')
+      })
   }
 
   return (
@@ -24,13 +29,29 @@ export default function Home() {
 
       <main>
         <Header title="Evropskoj Uniji trebaju VOZAČI sa Balkana!" />
-        <ReactPlayer url='https://youtu.be/q8xlOn4Atq8' controls={true} style={{maxWidth: '90%'}} />
-        <Link href="https://forms.gle/4zrXUPyEQAe8j1No7"><button id="apply" onClick={trackClick}>Prijavi se!</button></Link>
-        
+        <ReactPlayer
+          url="https://youtu.be/q8xlOn4Atq8"
+          controls={true}
+          style={{maxWidth: '90%'}}
+        />
+
+        <Link href="https://forms.gle/4zrXUPyEQAe8j1No7">
+          <button id="apply" onClick={trackClick} className="to">
+            Prijavi se!
+          </button>
+        </Link>
+        <CurvedArrow
+          fromSelector=".to"
+          toSelector=".to"
+          dynamicUpdate={true}
+          fromOffsetX={-200}
+          middleY={-100}
+          toOffsetX={-70}
+          toOffsetY={-25}
+        />
       </main>
 
       <Footer />
     </div>
   )
 }
-
